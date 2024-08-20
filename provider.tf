6e7051ca96136b7adbd0db441a1be505753f91fe
Naveen <naveenendu@gmail.com>
2024-08-20 21:24:51 +0530

    updated wor

--- provider.tf
+++ provider.tf
@@ -1,4 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-  
+  shared_config_files = "C:/Users/surie1998/.aws/config"
+  shared_credentials_files = "C:/Users/surie1998/.aws/credentials"
+  profile = "terraformuser"
 }

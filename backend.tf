06c74482aeeb9329344fe5eb5d5ebf5d283de9ce
Naveen <naveenendu@gmail.com>
2024-08-20 17:07:20 +0530

    my first commit

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "github-action-demo-23456"
+    key    = "github-action-demo-tfstate"
+    region = "us-east-1"
+    profile  = "terraformuser"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "buc" {
+  bucket = "my-tf-test-bucket24567"
+
+}
